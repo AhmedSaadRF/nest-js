93c192aef13cd37bbfdd00b1f76f6f8cbcd08d0a
Ahmed Saad <ahmedsaadalrefaey7@gmail.com>
2025-07-16 19:27:22 +0300

    Upload Project

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,12 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  sayWelcome(): string {
+    return 'Welcome to ';
+  }
+
+  sayWelcomeToUser(name: string): string {
+    return 'Welcome to ' + name + '!';
+  }
 }
