f62fc9a4c6bbe17724791f4e4e6e8c05cc720f90
Ahmed Saad <ahmedsaadalrefaey7@gmail.com>
2025-07-17 18:26:35 +0300

    Upload Project

--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -1,29 +1,52 @@
-/* eslint-disable prettier/prettier */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import { CreatePaymentDto } from './dto/create-payment.dto';
 import { UpdatePaymentDto } from './dto/update-payment.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Payment } from './entities/payment.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class PaymentService {
-  create(createPaymentDto: CreatePaymentDto) {
-    return 'This action adds a new payment';
+  constructor(
+    @InjectRepository(Payment) private paymentsRepository: Repository<Payment>,
+  ) {}
+
+  async create(createPaymentDto: CreatePaymentDto) {
+    const res = await this.paymentsRepository.save(createPaymentDto);
+    return res;
   }
 
   findAll() {
-    return `This action returns all payment`;
+    const payments = this.paymentsRepository.find();
+    return payments;
+    // return `This action returns all payment`;
   }
 
   findOne(id: number) {
     return `This action returns a #${id} payment`;
   }
 
-  update(id: number, updatePaymentDto: UpdatePaymentDto) {
-    return `This action updates a #${id} payment`;
+  async update(id: number, updatePaymentDto: UpdatePaymentDto) {
+    const existingPayment = await this.paymentsRepository.findOneBy({ id: id });
+
+    if (!existingPayment) {
+      throw new Error(`Payment with ID ${id} not found`);
+    }
+
+    // Update the payment properties
+    existingPayment.currency = updatePaymentDto.currency;
+    if (updatePaymentDto.amount !== undefined) {
+      existingPayment.amount = updatePaymentDto.amount;
+    }
+
+    const updatedPayment = await this.paymentsRepository.save(existingPayment);
+    return updatedPayment;
+    // return `This action updates a #${id} payment`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} payment`;
+  async remove(id: number) {
+    const deletedPayment = await this.paymentsRepository.delete(id);
+    return deletedPayment;
+    // return `This action removes a #${id} payment`;
   }
 }
